785e710f79a189bec1ca5c28df9af240e08ba3e8
Florent Tournois <florent.tournois@finances.gouv.fr>
2019-06-05 14:47:57 +0200

    Empty project !

---
+++ src/python/setup.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+# Copyright (c) 2018 Florent TOURNOIS
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+###############################################################################
+
+###############################################################################
+# @package pymdtools
+# Markdown Tools develops for Gucihet Entreprises
+#
+###############################################################################
+
+from setuptools import setup, find_packages
+
+import pymdtools
+
+# Ceci n'est qu'un appel de fonction. Mais il est trèèèèèèèèèèès long
+# et il comporte beaucoup de paramètres
+setup(
+
+    # le nom de votre bibliothèque, tel qu'il apparaitre sur pypi
+    name='upref',
+
+    # la version du code
+    version=pymdtools.__version__,
+
+    # Liste les packages à insérer dans la distribution
+    # plutôt que de le faire à la main, on utilise la foncton
+    # find_packages() de setuptools qui va cherche tous les packages
+    # python recursivement dans le dossier courant.
+    # C'est pour cette raison que l'on a tout mis dans un seul dossier:
+    # on peut ainsi utiliser cette fonction facilement
+    packages=find_packages(),
+
+    # votre pti nom
+    author=pymdtools.__author__,
+
+    # Votre email, sachant qu'il sera publique visible, avec tous les risques
+    # que ça implique.
+    author_email=pymdtools.__email__,
+
+    # Une description courte
+    description=pymdtools.__doc__,
+
+    # Une description longue, sera affichée pour présenter la lib
+    # Généralement on dump le README ici
+    long_description=pymdtools.__doc__,
+
+    # Vous pouvez rajouter une liste de dépendances pour votre lib
+    # et même préciser une version. A l'installation, Python essayera de
+    # les télécharger et les installer.
+    #
+    # Ex: ["gunicorn", "docutils >= 0.3", "lxml==0.5a7"]
+    #
+    # Dans notre cas on en a pas besoin, donc je le commente, mais je le
+    # laisse pour que vous sachiez que ça existe car c'est très utile.
+    # install_requires= ,
+
+    # Active la prise en compte du fichier MANIFEST.in
+    include_package_data=True,
+
+    # Une url qui pointe vers la page officielle de votre lib
+    url='https://github.com/IIXIXII/upref',
+
+    # Il est d'usage de mettre quelques metadata à propos de sa lib
+    # Pour que les robots puissent facilement la classer.
+    # La liste des marqueurs autorisées est longue:
+    # https://pypi.python.org/pypi?%3Aaction=list_classifiers.
+    #
+    # Il n'y a pas vraiment de règle pour le contenu. Chacun fait un peu
+    # comme il le sent. Il y en a qui ne mettent rien.
+    classifiers=[
+        "Programming Language :: Python",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.5",
+    ],
+
+
+    # C'est un système de plugin, mais on s'en sert presque exclusivement
+    # Pour créer des commandes, comme "django-admin".
+    # Par exemple, si on veut créer la fabuleuse commande "proclame-sm", on
+    # va faire pointer ce nom vers la fonction proclamer(). La commande sera
+    # créé automatiquement.
+    # La syntaxe est "nom-de-commande-a-creer = package.module:fonction".
+    # entry_points={
+    #     'console_scripts': [
+    #         'proclame-sm = sm_lib.core:proclamer',
+    #     ],
+    # },
+
+    # A fournir uniquement si votre licence n'est pas listée dans "classifiers"
+    # ce qui est notre cas
+    license=pymdtools.__license__,
+
+    # Il y a encore une chiée de paramètres possibles, mais avec ça vous
+    # couvrez 90% des besoins
+
+)
